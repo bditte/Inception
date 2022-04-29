866c1d8c2b34aa83a7210181464ad0e1ad0db041
bditte <bditte@student.42.fr>
2022-04-29 09:46:35 +0200

    push from home

--- inception/srcs/requirements/nginx/config/setup.sh
+++ inception/srcs/requirements/nginx/config/setup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-sed -i s/SERVER_NAME/${DOMAIN_NAME}/g /etc/nginx/conf.d/default.conf
-sed -i s/CERTS_DIR/${CERTS_DIR}/g /etc/nginx/conf.d/default.conf
-sed -i s/TLS_VERSION/${TLS_VERSION}/g /etc/nginx/conf.d/default.conf
+sed -i "s/SERVER_NAME/${DOMAIN_NAME}/g" /etc/nginx/sites-available/default;
+sed -i "s/CERTS_DIR/${CERTS_DIR}/g" /etc/nginx/sites-available/default;
+sed -i "s/TLS_VERSION/${TLS_VERSION}/g" /etc/nginx/sites-available/default;
 
-sudo nginx -g "daemon off";
+if [ ! -f /etc/ssl/bditte.pem ]; then
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
+      -out /etc/ssl/bditte.pem \
+      -keyout /etc/ssl/bditte.key \
+      -subj "/C=FR/ST=Paris/L=Paris/O=42/CN=${DOMAIN_NAME}/"
+fi
+
+exec "$@"
---
+++ inception/srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+	chown -R www-data:www-data /var/www/*;
+	chown -R 755 /var/www/*;
+	mkdir -p /run/php/;
+	touch /run/php/php7.3-fpm.pid;
+
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+	mkdir -p /var/www/wordpress
+	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+	chmod +x wp-cli.phar; 
+	mv wp-cli.phar /usr/local/bin/wp;
+	cd /var/www/wordpress;
+	wp core download --allow-root;
+	mv /var/www/wp-config.php /var/www/wordpress/
+	wp core install --allow-root --url=${DOMAIN_NAME} --title="${WP_TITLE}" --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
+fi
+
+exec "$@"
