856d3b4c196df28a71bc1853744bb929ca48c9b0
bditte <bditte@student.42.fr>
2022-05-03 16:09:06 +0200

    need to fix comment

--- inception/srcs/requirements/nginx/tools/setup.sh
+++ inception/srcs/requirements/nginx/tools/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i "s/SERVER_NAME/${DOMAIN_NAME}/g" /etc/nginx/sites-available/default;
+sed -i "s/DOMAIN_NAME/${DOMAIN_NAME}/g" /etc/nginx/sites-available/default;
 sed -i "s/CERTS_DIR/${CERTS_DIR}/g" /etc/nginx/sites-available/default;
 sed -i "s/TLS_VERSION/${TLS_VERSION}/g" /etc/nginx/sites-available/default;
 
--- inception/start.sh
+++
@@ -1,1 +0,0 @@
-docker rm "nginx-test" && docker rmi mynginx ; docker build -t "mynginx" . && docker run -i -p 80:80 --name nginx-test mynginx;
