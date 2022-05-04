77467a2ca563f0648ee831a77e0538424b6d1dee
bditte <bditte@student.42.fr>
2022-05-04 10:30:01 +0200

    final commit

--- inception/srcs/requirements/mariadb/tools/start.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'bditte'@'%' IDENTIFIED BY 'bditte_password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'bditte'@'%';
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root_password';
